5282e8b56bbb6f1ebe2771be452f9f8aff04aeb8
U77w41 <U77w41@gmail.com>
2022-09-01 16:07:00 +0530

    Updated the listen.py file

--- listen.py
+++ listen.py
@@ -1,4 +1,15 @@
 import speech_recognition as sr
 
 def Listen():
-    
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('Listening....')
+        r.pause_threshold =1
+        audio = r.listen(source, 0,2)
+    try:
+        print("Recognizing.....")
+        query = r.recognize_google(audio ,language = 'en-in')
+        print(f'You said : {query}')
+    except:
+        return
