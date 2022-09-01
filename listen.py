eee0a071186a190cc11e03ae1c6fc4a333d01e09
U77w41 <U77w41@gmail.com>
2022-09-01 16:23:35 +0530

    Add the model .py

--- listen.py
+++ listen.py
@@ -6,7 +6,7 @@ def Listen():
     with sr.Microphone() as source:
         print('Listening....')
         r.pause_threshold =1
-        audio = r.listen(source, 0,2)
+        audio = r.listen(source, 0,5)  # change 5 to any integer to change for how many times the assistant listen
     try:
         print("Recognizing.....")
         query = r.recognize_google(audio ,language = 'en-in')
