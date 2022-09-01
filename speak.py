bb796b72521c0109997bbb535ac7bb652f432651
U77w41 <U77w41@gmail.com>
2022-09-01 12:07:51 +0530

    updated the speak.py file

--- speak.py
+++ speak.py
@@ -3,7 +3,7 @@ import pyttsx3
 
 engine = pyttsx3.init("sapi5")  # The speech engine sapi5 created my microsoft
 voices = engine.getProperty('voices') # storing the voices inside a variable
-engine.setProperty('voices',voices[0].id) # setting the voice id 0 ,1 ,2 .....
+engine.setProperty('voices',voice[0].id) # setting the voice id 0 ,1 ,2 .....
 engine.setProperty('rate',170) # speech rate 200 as a normal speed
 
 # Creating a function to speak a given text
